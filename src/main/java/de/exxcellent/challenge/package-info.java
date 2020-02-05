52c9c87d1839fb82d394e70424b2c1f3d4be4afa
W K <krauswg@users.noreply.github.com>
2020-02-05 19:32:03 +0100

    Eclipse Style formatting

--- src/main/java/de/exxcellent/challenge/package-info.java
+++ src/main/java/de/exxcellent/challenge/package-info.java
@@ -1,7 +1,7 @@
 /**
  * The root package for the data challenge containing the entry point
- * {@link de.exxcellent.challenge.App} and containing the resource files.
- * Please create and document your own package structure appropriate to your design.
+ * {@link de.exxcellent.challenge.App} and containing the resource files. Please
+ * create and document your own package structure appropriate to your design.
  *
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -7,25 +7,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Example JUnit 5 test case.
+ * 
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
 class AppTest {
 
-    private String successLabel = "not successful";
+	private String successLabel = "not successful";
 
-    @BeforeEach
-    void setUp() {
-        successLabel = "successful";
-    }
+	@BeforeEach
+	void setUp() {
+		successLabel = "successful";
+	}
 
-    @Test
-    void aPointlessTest() {
-        assertEquals("successful", successLabel, "My expectations were not met");
-    }
+	@Test
+	void aPointlessTest() {
+		assertEquals("successful", successLabel, "My expectations were not met");
+	}
 
-    @Test
-    void runFootball() {
-        App.main("--football", "football.csv");
-    }
+	@Test
+	void runFootball() {
+		App.main("--football", "football.csv");
+	}
+
 
 }
