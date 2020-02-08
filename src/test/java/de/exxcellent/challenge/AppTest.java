e763add49695a396cbd3f405fbc519837f6439e8
W K <krauswg@users.noreply.github.com>
2020-02-08 15:23:03 +0100

    Validating the output to the console of the main entry point with the given test data

--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -1,7 +1,13 @@
 package de.exxcellent.challenge;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -11,9 +17,28 @@ import org.junit.jupiter.api.Test;
  */
 class AppTest {
 
+	private static final PrintStream stdOut = System.out;
+	private static final PrintStream stdErr = System.err;
+	private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private static final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+	@BeforeAll
+	public static void setUpStreams() {
+		System.setOut(new PrintStream(outContent));
+		System.setErr(new PrintStream(errContent));
+	}
+
+	@AfterAll
+	public static void restoreStreams() {
+		System.setOut(stdOut);
+		System.setErr(stdErr);
+	}
+
 	@Test
 	void runFootball() {
 		App.main("--football", "src/main/resources/de/exxcellent/challenge/football.csv");
+		assertTrue(outContent.toString().contains("Team with smallest goal spread       : Aston_Villa"),
+				"Expected team not present in the output.");
 	}
 
 	@Test
@@ -24,6 +49,8 @@ class AppTest {
 	@Test
 	void runWeather() {
 		App.main("--weather", "src/main/resources/de/exxcellent/challenge/weather.csv");
+		assertTrue(outContent.toString().contains("Day with smallest temperature spread : 14"),
+				"Expected team not present in the output.");
 	}
 
 	@Test
