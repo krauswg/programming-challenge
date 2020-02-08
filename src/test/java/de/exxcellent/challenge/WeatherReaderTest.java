b4026be183c0b7d1d579fea1fd71fa197e5cad0e
W K <krauswg@users.noreply.github.com>
2020-02-08 13:02:16 +0100

    Test for the used values and one double from the entries
    Test for prohibited reuse

--- src/test/java/de/exxcellent/challenge/WeatherReaderTest.java
+++ src/test/java/de/exxcellent/challenge/WeatherReaderTest.java
@@ -75,7 +75,51 @@ public class WeatherReaderTest {
 		testReader(file, lenient, expectedRecords, expectedErrors);
 	}
 
-	private void testReader(String filename, boolean lenient, int expectedRecords, int expectedErrors) {
+	@Test
+	public void testReaderValues() {
+		String file = "target/classes/de/exxcellent/challenge/weather-strict.csv";
+		boolean lenient = false;
+		int expectedRecords = 1;
+		int expectedErrors = 0;
+		List<WeatherRecord> csvEntry = testReader(file, lenient, expectedRecords, expectedErrors);
+		WeatherRecord record = csvEntry.get(0);
+		// 1,88,59,74,53.8,0,280,9.6,270,17,1.6,93,23,1004.5
+		assertEquals(88, record.getMxT(), "Max temperature was not as expected!");
+		assertEquals(59, record.getMnT(), "Max temperature was not as expected!");
+		assertEquals(1004.5d, record.getR_AvSLP(), "R AvSLP was not as expected!");
+	}
+
+	@Test
+	public void testReuse() {
+		ICSVReader<WeatherRecord> reader = getTestReader();
+		String filename = "target/classes/de/exxcellent/challenge/weather-strict.csv";
+		InputStream input = null;
+		File file = new File(filename);
+		try {
+			input = new FileInputStream(file);
+		} catch (FileNotFoundException e) {
+			fail("File for testing was not found: " + file.getAbsolutePath(), e);
+		}
+		reader.getCSVEntries(input, WeatherRecord::new, ",", Locale.ENGLISH, true);
+		try {
+			reader.getCSVEntries(input, WeatherRecord::new, ",", Locale.ENGLISH, true);
+		} catch (IllegalStateException e) {
+			// expected exception
+			return;
+		}
+		fail("Expected exception was not thrown!");
+	}
+
+	/**
+	 * Reads the entries from the file and asserts the expected errors/entry count
+	 * 
+	 * @param filename
+	 * @param lenient
+	 * @param expectedRecords
+	 * @param expectedErrors
+	 * @return the successfully read records if the counts match
+	 */
+	private List<WeatherRecord> testReader(String filename, boolean lenient, int expectedRecords, int expectedErrors) {
 		ICSVReader<WeatherRecord> reader = getTestReader();
 
 		InputStream input = null;
@@ -88,5 +132,7 @@ public class WeatherReaderTest {
 		List<WeatherRecord> csvEntries = reader.getCSVEntries(input, WeatherRecord::new, ",", Locale.ENGLISH, lenient);
 		assertEquals(expectedRecords, csvEntries.size(), "Unexpected record count of the parser.");
 		assertEquals(expectedErrors, reader.getLinesWithError().length, "Unexpected error count of the parser.");
+		assertEquals(expectedErrors > 0, reader.hasError(), "Unexpected state of hasError.");
+		return csvEntries;
 	}
 }
